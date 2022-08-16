c9763c2b521f7bcdb668acb4eef90e4a3b3e035e
phinic925 <phinicnyangate480@gmail.com>
2022-08-16 09:03:25 +0100

    commit tree 5630185fc4619eb3c24ab181d4a0b80c4ccfbb3b

--- methods.rb
+++ methods.rb
@@ -1,1 +1,28 @@
 # Your code here!
+def greetProgrammer
+    puts "Hello, programmer!"
+end
+
+
+def greet(name)
+    puts "Hello, #{name}!"
+    end
+   
+
+    def greet_with_default(name="programmer")
+        puts "Hello, #{name}!"
+    end
+
+    def add (num1,num2)
+        return num1+num2
+    end
+
+    def halve(num)
+        if num.class != Integer
+             return nil;
+        end
+        return num / 2
+        end
+
+
+
